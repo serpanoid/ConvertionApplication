d77eca672adb85340c9e1280fa048e025af615fa
Nikita <cloudfreze102@gmail.com>
2022-10-09 17:03:08 +0300

    Second version
    Added grams into liters
    Added liters to get galloons

--- ConvertionApp/Program.cs
+++ ConvertionApp/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine($"Meters to cm: {int.Parse(Console.ReadLine()) * 100} cm");
 Console.WriteLine("Enter amount in meters to get km:");
 Console.WriteLine($"Meters to km: {double.Parse(Console.ReadLine()) / 1000} km");
+
+Console.WriteLine("Enter amount in grams to get liters:");
+Console.WriteLine($"Grams to liters: {int.Parse(Console.ReadLine()) / 1000} cm");
+Console.WriteLine("Enter amount in liters to get galloons:");
+Console.WriteLine($"Liters to galloons: {double.Parse(Console.ReadLine()) / 3.8} km");
